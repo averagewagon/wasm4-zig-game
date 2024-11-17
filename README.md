29a405833d200dbf02023443f9554ac923df95f2
Archit Gupta <archit@accelbread.com>
2024-11-17 13:20:23 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# game
+
+To be determined.
---
+++ build.zig
@@ -0,0 +1,54 @@
+// game - To be determined
+// Copyright (C) 2024 Archit Gupta <archit@accelbread.com>
+// Copyright (C) 2024 Jonathan Hendrickson <jonathan@jhendrickson.dev>
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+const std = @import("std");
+const builtin = @import("builtin");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{
+        .preferred_optimize_mode = .ReleaseSafe,
+    });
+
+    const exe_opts = .{
+        .name = "game",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+        .strip = true,
+    };
+
+    const exe = b.addExecutable(exe_opts);
+
+    b.installArtifact(exe);
+
+    const unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
+
+    const exe_check = b.addExecutable(exe_opts);
+    const check_step = b.step("check", "Check if app compiles");
+    check_step.dependOn(&exe_check.step);
+    check_step.dependOn(&unit_tests.step);
+}
